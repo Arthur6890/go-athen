c29592f25bea9c13b130724b02bcf20c457e5a49
Arthur Amaral <arthur.ramos.amaral@hotmail.com>
2022-04-10 21:08:27 -0300

    alteracao no header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,46 +1,94 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { AtenaLogo } from "../../assets/svg/index";
+import AtenaLogoImg from "../../assets/images/SIMBOLO2.png";
 import banner from "../../assets/images/banner2.png";
 import styles from "./styles.module.scss";
 
-function Header() {
+type props = {
+  hide?: boolean;
+  title?: string;
+};
+
+function Header({ hide, title }: props) {
   return (
-    <div className={styles.main}>
-      <div className={styles.image}>
-        <img src={banner} alt="" />
-        <div className={styles.blurBackground}>
-          <h1>#VemPraAtena</h1>
+    <>
+      {!hide && (
+        <div className={styles.main}>
+          <div className={styles.image}>
+            <img src={banner} alt="" />
+            <div className={styles.blurBackground}>
+              <h1>{title}</h1>
+            </div>
+          </div>
+          <div className={styles.content}>
+            <AtenaLogo />
+            <div className={styles.sections}>
+              <Link to="/" replace>
+                Home
+              </Link>
+              <Link to="/about" replace>
+                Quem Somos
+              </Link>
+              <Link to="/sports">Esportes</Link>
+              <a
+                href="https://www.instagram.com/aaaicex/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Siga-nos
+              </a>
+              <a
+                id={styles.giant}
+                target="_blank"
+                rel="noreferrer"
+                href="https://docs.google.com/forms/d/e/1FAIpQLSdLHowQOZFoNzgxAib62kSQozGBP4Y6hbqiwJZJ-XwkznNxSQ/viewform"
+              >
+                Seja um gigante!
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className={styles.content}>
-        <AtenaLogo />
-        <div className={styles.sections}>
-          <Link to="/" replace>
-            Home
-          </Link>
-          <Link to="/about" replace>
-            Quem Somos
-          </Link>
-          <Link to="/sports">Esportes</Link>
-          <a
-            href="https://www.instagram.com/aaaicex/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Siga-nos
-          </a>
-          <a
-            id={styles.giant}
-            target="_blank"
-            rel="noreferrer"
-            href="https://docs.google.com/forms/d/e/1FAIpQLSdLHowQOZFoNzgxAib62kSQozGBP4Y6hbqiwJZJ-XwkznNxSQ/viewform"
-          >
-            Seja um gigante!
-          </a>
+      )}
+
+      {hide && (
+        <div className={styles.main}>
+          <div className={`${styles.content} ${styles.hidenContent}`}>
+            <div className={`${styles.sections} ${styles.hidenSections}`}>
+              <span>
+                <img src={AtenaLogoImg} alt="" />
+              </span>
+              <div>
+                <Link to="/" replace>
+                  Home
+                </Link>
+                <Link to="/about" replace>
+                  Quem Somos
+                </Link>
+                <Link to="/sports" className={styles.notHiden}>
+                  Esportes
+                </Link>
+                <a
+                  href="https://www.instagram.com/aaaicex/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Siga-nos
+                </a>
+                <a
+                  id={styles.giant}
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://docs.google.com/forms/d/e/1FAIpQLSdLHowQOZFoNzgxAib62kSQozGBP4Y6hbqiwJZJ-XwkznNxSQ/viewform"
+                >
+                  Seja um gigante!
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,26 +1,30 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import Header from "../../components/header";
-import Slider from "../../components/carrousel";
+import Banner from "../../assets/images/banner.png";
 
 function Home() {
   return (
-    <div className={styles.home}>
-      <Header />
-      <Slider />
-      <h1 className={styles.title}>Aqui vai ser a pagina incial.</h1>
-      <h3 className={styles.title}>
-        Todos os links do menu (
-        <strong>
-          "Home", "Quem somos", "Esportes", "Siga-nos" e "Seja um gigante"
-        </strong>
-        ) estão funcionando. Experimente clica-los!
-      </h3>
-      <h4 className={`${styles.title} ${styles.last}`}>
-        As imagens que vao ficar alternando a gnt ve se vc faz algumas artes, ou
-        se poe foto dos times, por enquanto pus essas como exemplo também.
-      </h4>
-    </div>
+    <>
+      <Header hide />
+      <img src={Banner} alt="a3" className={styles.image} />
+
+      <section className={styles.aboutUs}>
+        <h1 className={styles.title}>tetet</h1>
+        <h1 className={styles.title}>tetet</h1>
+        <h1 className={styles.title}>tetet</h1>
+        <h1 className={styles.title}>tetet</h1>
+        <h1 className={styles.title}>tetet</h1>
+        <h1 className={styles.title}>tetet</h1>
+        <h1 className={styles.title}>tetet</h1>
+        <h1 className={styles.title}>tetet</h1>
+        <h1 className={styles.title}>tetet</h1>
+        <h1 className={styles.title}>tetet</h1>
+        <h1 className={styles.title}>tetet</h1>
+        <h1 className={styles.title}>tetet</h1>
+        <h1 className={styles.title}>tetet</h1>
+      </section>
+    </>
   );
 }
 
